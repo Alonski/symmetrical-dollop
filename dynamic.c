4f67b8bf0895198e6cad36fd71f018fd0452a23e
Alon <alon@camilyo.com>
2020-08-16 17:44:21 +0300

    feat: Clean up dynamic

--- dynamic.c
+++ dynamic.c
@@ -24,7 +24,24 @@ struct Point
 void masterProcess(int num_procs);
 void slaveProcess();
 int generateNumbers(struct Point *arr, int size);
-double heavy(int x, int y);
+void sendInitialJobs(struct Point *tasks, int num_procs);
+double receiveAndSendWorkJobs(struct Point *tasks, int num_procs, int jobs_to_do);
+double receiveFinalEndJobs(struct Point *tasks, int num_procs);
+
+// This function performs heavy computations,
+// its run time depends on x and y values
+double heavy(int x, int y)
+{
+    int i, loop = SHORT;
+    double sum = 0;
+    // Super heavy tasks
+    if (x < 3 || y < 3)
+        loop = LONG;
+    // Heavy calculations
+    for (i = 0; i < loop * HEAVY; i++)
+        sum += cos(exp(sin((double)i / HEAVY)));
+    return sum;
+}
 
 int main(int argc, char *argv[])
 {
@@ -49,27 +66,10 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-// This function performs heavy computations,
-// its run time depends on x and y values
-double heavy(int x, int y)
-{
-    int i, loop = SHORT;
-    double sum = 0;
-    // Super heavy tasks
-    if (x < 3 || y < 3)
-        loop = LONG;
-    // Heavy calculations
-    for (i = 0; i < loop * HEAVY; i++)
-        sum += cos(exp(sin((double)i / HEAVY)));
-    return sum;
-}
-
 void masterProcess(int num_procs)
 {
-    MPI_Status status;
-
-    double all_sum = 0, worker_sum = 0, start_time = MPI_Wtime();
-    int worker_id, x, y, source, tag, jobs_sent = 0, i = 0, arr_size = N * N;
+    double all_sum = 0, start_time = MPI_Wtime();
+    int arr_size = N * N;
 
     // Set jobs to do == arr_size as each process will run a single task at a time
     int jobs_to_do = arr_size;
@@ -78,37 +78,9 @@ void masterProcess(int num_procs)
     struct Point *tasks = (struct Point *)malloc(sizeof(struct Point) * arr_size);
     generateNumbers(tasks, N);
 
-    for (worker_id = 1; worker_id < num_procs; worker_id++)
-    {
-        // Send the first task to each process
-        MPI_Send(tasks + (worker_id - 1), sizeof(struct Point), MPI_INT, worker_id, WORK_TAG, MPI_COMM_WORLD);
-    }
+    sendInitialJobs(tasks, num_procs);
 
-    for (jobs_sent = num_procs - 1; jobs_sent < jobs_to_do; jobs_sent++)
-    {
-        if (jobs_to_do - jobs_sent > num_procs - 1)
-            tag = WORK_TAG;
-        else
-            tag = END_TAG;
-
-        // Receive a sum for a task from a process that isn't finished
-        MPI_Recv(&worker_sum, 1, MPI_DOUBLE, MPI_ANY_SOURCE, WORK_TAG, MPI_COMM_WORLD, &status);
-
-        source = status.MPI_SOURCE;
-
-        // Send another task to the received process
-        MPI_Send(tasks + jobs_sent, sizeof(struct Point), MPI_INT, source, tag, MPI_COMM_WORLD);
-
-        all_sum += worker_sum;
-    }
-
-    for (worker_id = 1; worker_id < num_procs; worker_id++)
-    {
-        // Receive a sum for a task from a process that is finished
-        MPI_Recv(&worker_sum, 1, MPI_DOUBLE, MPI_ANY_SOURCE, END_TAG, MPI_COMM_WORLD, &status);
-
-        all_sum += worker_sum;
-    }
+    all_sum = receiveAndSendWorkJobs(tasks, num_procs, jobs_to_do) + receiveFinalEndJobs(tasks, num_procs);
 
     // Print the answers and the total time taken
     printf("Dynamic answer = %e - Time %lf\n", all_sum, MPI_Wtime() - start_time);
@@ -148,4 +120,52 @@ int generateNumbers(struct Point *arr, int size)
             i++;
         }
     }
+}
+
+void sendInitialJobs(struct Point *tasks, int num_procs)
+{
+    for (int worker_id = 1; worker_id < num_procs; worker_id++)
+    {
+        // Send the first task to each process
+        MPI_Send(tasks + (worker_id - 1), sizeof(struct Point), MPI_INT, worker_id, WORK_TAG, MPI_COMM_WORLD);
+    }
+}
+
+double receiveAndSendWorkJobs(struct Point *tasks, int num_procs, int jobs_to_do)
+{
+    MPI_Status status;
+    double all_sum = 0, worker_sum = 0;
+    int tag, source;
+    for (int jobs_sent = num_procs - 1; jobs_sent < jobs_to_do; jobs_sent++)
+    {
+        if (jobs_to_do - jobs_sent > num_procs - 1)
+            tag = WORK_TAG;
+        else
+            tag = END_TAG;
+
+        // Receive a sum for a task from a process that isn't finished
+        MPI_Recv(&worker_sum, 1, MPI_DOUBLE, MPI_ANY_SOURCE, WORK_TAG, MPI_COMM_WORLD, &status);
+
+        source = status.MPI_SOURCE;
+
+        // Send another task to the received process
+        MPI_Send(tasks + jobs_sent, sizeof(struct Point), MPI_INT, source, tag, MPI_COMM_WORLD);
+
+        all_sum += worker_sum;
+    }
+    return all_sum;
+}
+
+double receiveFinalEndJobs(struct Point *tasks, int num_procs)
+{
+    MPI_Status status;
+    double all_sum = 0, worker_sum = 0;
+    for (int worker_id = 1; worker_id < num_procs; worker_id++)
+    {
+        // Receive a sum for a task from a process that is finished
+        MPI_Recv(&worker_sum, 1, MPI_DOUBLE, MPI_ANY_SOURCE, END_TAG, MPI_COMM_WORLD, &status);
+
+        all_sum += worker_sum;
+    }
+    return all_sum;
 }
